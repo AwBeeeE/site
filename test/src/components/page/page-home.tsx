5f0a72d8991d1c8fb98f5191a27b6c9ca2708a0a
AwBeeeE <louis_562@hotmail.com>
2021-07-19 21:44:00 +0900

    first commit

---
+++ test/src/components/page/page-home.tsx
@@ -0,0 +1,219 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'page-home',
+})
+export class PageHome {
+    render() {
+        return (
+            <div>
+                <div class="header1">
+                    <div id="fv_button">
+                        <stencil-route-link url="#apply"> <img src="/assets/images/fv_btn.png" id="fvbtn"></img> </stencil-route-link>
+                    </div>
+                </div>
+                <div class="home">
+                    <p>※当セミナーは、YouTubeで集客や売上を上げる方法が学べる経営者・個人事業主・自営業者向けの内容です。
+                        <br></br>
+                        YouTuberとして広告収入を得る目的や動画編集スキルを身に付けたい方向けではございません。</p>
+                    <div class="icon">
+                        <p><font size="25">ビジネスYouTubeに、</font></p>
+                        <p><font size="25" color="#FFC0CB">再生回数は必要ありません！</font></p>
+                        <img src="/assets/images/title_bottom.png" class="tb"></img>
+                    </div>
+                    <div class="middle2">
+                        <p>動画市場が伸びている今、ビジネスにおいてYouTubeを利用することが必須になってきています<br></br>
+                            そんな中、YouTubeを始めたいと思っている方や実際に始めてみた方も多いのではないでしょうか？<br></br>
+                            しかし、取り組み方がわからずに始められていなかったり、実際にやってみたとしても、<br></br>
+                            全く成果に繋がらない方がほとんどです。<br></br>
+                            なぜなら、「ビジネスYouTube」のためのノウハウを知らないから。
+                        </p>
+                    </div>
+                    <div class="middle3">
+                        <div class="line"></div>
+                        <img src="/assets/images/img_0001_Layer-3.png" id="buddy"></img>
+                        <div class="log">
+                            実は、ビジネスにおいて集客・売上アップのためにYouTubeを活用する「ビジネスYouTube」には、一般的なYouTuberなどとは全く異なる<h class="spword">ノウハウ</h>があるんです。
+                            <br></br><br></br>
+                            <img src="/assets/images/red_check.png"></img>再生回数を稼げる面白かったりクオリティの高い動画の企画や編集スキル<br></br>
+                            <img src="/assets/images/red_check.png"></img>容姿やトークなどに秀でたカリスマ的なキャラクター<br></br>
+                            <img src="/assets/images/red_check.png"></img>競合と差別化された特別な内容<br></br>
+                            <font size="22" color="#F08080" >ビジネスYouTubeでは、どれも必要ありません!</font>
+                        </div>
+                    </div>
+                    <div class="middle2">
+                        <p><font color="#F08080">ビジネスYouTubeにおいて最も大切なのは、動画そのものや再生回数ではなく、<br></br>
+                            YouTubeチャンネルの設定や運営等の<h class="spword">マーケティング</h>です。</font><br></br>
+                            このマーケティングを習得し、実践すれば、誰でも簡単にYouTubeから自身のビジネスに<br></br>
+                            顧客を呼び込み、売上を上げることが可能なんです。</p>
+                    </div>
+                    <div>
+                        <p>実際にビジネスYouTubeで集客・売上アップを達成した事例をご紹介します。</p>
+                        <div class="line"></div>
+                        <div class="arrow"></div>
+                        <div class="pics">
+                            <img src="/assets/images/example1.png"></img>
+                            <   img src="/assets/images/example2.png"></img>
+                            <img src="/assets/images/example3.png"></img>
+                        </div>
+                    </div>
+                    <div class="middle4">
+                        <div class="line"></div>
+                        <br></br>
+                        <p>他にも、飲食店、セミナー講師、税理士、弁護士、コンサルタント、映像制作会社、スタイリスト、投資塾、<br></br>
+                            不動産営業、健康食品、整体院、エステ、美容室、アパレル等・・・様々な業界で実績が出ています。</p>
+                    </div>
+                    <div class="middle2">
+                        <p>
+                            一般的なYouTuber関連のノウハウと異なり、ビジネスYouTubeに関するノウハウは、<br></br>
+                            まだあまり知られておらず、それを教えられる人も限られています。<br></br>
+                            このセミナーでは、そんなビジネスYouTubeに精通した講師が、その専門的なノウハウを具体的にお伝えします。<br></br>
+                            基本的な考え方から実践方法まで詳しくお伝えしますので、参加当日から取り組んでいただけます。<br></br>
+                        </p>
+                        <stencil-route-link url="#apply"> <img src="/assets/images/fv_btn.png" class="fvbtn2"></img> </stencil-route-link>
+                    </div>
+                </div>
+                <div class="lastpart">
+                    <div class="middle5">
+                        <div class="icon1">
+                            <p><font size="25">このセミナーで学べる内容</font></p>
+                            <img src="/assets/images/title_bottom.png" class="tb"></img>
+                            <div class="log">
+                                <p><font color="white">実際に集客・売上が上がるYouTubeチャンネルの作り方<br></br>
+                                    <img src="/assets/images/check.png"></img>無料で多くの人に動画を見てもらう方法<br></br>
+                                    <img src="/assets/images/check.png"></img>YouTube活用で多くの人がハマる落とし穴<br></br>
+                                    <img src="/assets/images/check.png"></img>YouTubeに自分の動画やチャンネルをオススメしてもらう方法<br></br>
+                                    <img src="/assets/images/check.png"></img>YouTubeから売上に繋げるための最新テクニック</font></p>
+                                <stencil-route-link url="#apply"> <img src="/assets/images/fv_btn.png" class="fvbtn2"></img> </stencil-route-link>
+                            </div>
+                            <div class="icon2">
+                                <img src="/assets/images/title_bottom.png" class="tb"></img>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="middle6">
+                        <div class="line1"></div>
+                        <h class="title">オープニング</h>
+                        <ul class="a">
+                            <li>講師・会社紹介</li>
+                            <li>セミナー主旨・概要</li>
+                        </ul>
+                        <img src="/assets/images/program1.jpg" class="examples"></img>
+                        <br></br>
+                        <img src="/assets/images/triangle_border.png"></img>
+                        <h class="tnum">第 1 部</h>
+                        <h class="title">   YouTubeの基礎知識</h>
+                        <ul class="a">
+                            <li>なぜ今ビジネスYouTubeなのか</li>
+                            <li>集客ツールとしてのYouTubeの立ち位置</li>
+                            <li>ビジネスYouTubeに取り組む上で外せないポイント</li>
+                        </ul>
+                        <img src="/assets/images/program2.jpg" class="examples"></img>
+                        <br></br>
+                        <img src="/assets/images/triangle_border.png"></img>
+                        <h class="tnum">第 2 部</h>
+                        <h class="title">   売れるビジネスYouTubeの実践方法&amp;成功事例</h>
+                        <ul class="a">
+                            <li>売れるYouTubeチャンネルの作り方</li>
+                            <li>動画SEO最新テクニック</li>
+                            <li>集客・売上アップに止まらないビジネスYouTubeの可能性</li>
+                            <li>成功事例紹介</li>
+                        </ul>
+                        <img src="/assets/images/program3.jpg" class="specex"></img>
+                        <br></br>
+                        <img src="/assets/images/triangle_border.png"></img>
+                        <h class="tnum">第 3 部</h>
+                        <h class="title">   質疑応答・個別相談</h>
+                        <ul class="a">
+                            <li>質疑応答</li>
+                            <li>無料個別面談（ご希望の方）</li>
+                        </ul>
+                        <img src="/assets/images/program4.jpg" class="examples"></img>
+                    </div>
+                    <div id="apply">
+                        <div class="icon3">
+                            <p><font size="25">お申し込みフォーム</font></p>
+                            <img src="/assets/images/title_bottom.png" class="tb"></img>
+                        </div>
+                        <p>参加されたい日時のボタンをクリックし、フォームに記入し送信してください。<br></br>
+                            ※お一人様1回までの参加とさせていただきます。</p>
+                    </div>
+                    <div class="appinfo">
+                        <ol class="b">
+                            <li> 参加されたい日時のボタンを押してください。</li>
+                            <li>  申し込みフォームが表示されますので、必要事項を記入し送信してください。</li>
+                            <li>会場の案内図や当日の詳細情報ををメールにてお送りします。</li>
+                        </ol>
+                    </div>
+                    <div class="cal">
+                        <iframe src="https://calendar.google.com/calendar/embed?src=eagleeyeawp%40gmail.com&ctz=Asia%2FTokyo" id="gcal"></iframe>
+                    </div>
+                    <div class="icon4">
+                        <p><font size="25">よくある質問</font></p>
+                        <img src="/assets/images/title_bottom.png" class="tb"></img>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <h class="eqf">Q 参加するのに必要な知識や資格はありますか？</h><br></br>
+                        <br></br>
+                        <h class="eaf">A 社長、経営者、個人事業主、自営業者の方限定のセミナーとなっております。</h>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <h class="eqf">Q 強引にセミナー内で何らかの購入を勧められることはありませんか？</h><br></br>
+                        <br></br>
+                        <h class="eaf">A いいえ。強引な販売は一切行っておりませんので、ご安心してご参加ください。興味のある方には、必要に応じて商品販売、コンテンツ提供を行う場合があることをご了承ください。</h>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <h class="eqf">Q セミナーに参加したら必ずビジネスYouTubeを始めなくてはならないですか？</h><br></br>
+                        <br></br>
+                        <h class="eaf">A 活動を始めるか始めないかについては、100％参加者の方のご判断にお任せしています。こちらから活動開始を強要するようなことは一切ございませんので、受講後にご自身でお好きな方をご選択ください。</h>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <h class="eqf">Q 当日遅れての参加は可能ですか？</h><br></br>
+                        <br></br>
+                        <h class="eaf">A 申し訳ございません。他の参加者の方が集中するためにも、開始15分以降の遅刻に関しましてはご参加をお断りしております。予めご了承のほどよろしくお願いいたします。</h>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <h class="eqf">Q オンラインでの参加の流れを教えて下さい</h><br></br>
+                        <br></br>
+                        <h class="eaf">Aオンライン会議システムzoomを使用してのオンラインセミナーとなっております。開催日が近くなりましたら、zoomのリンクをお送り致しますので、そのリンクをクリックいただければご参加可能です。※スマートフォンでのご参加の場合、事前にzoomアプリのダウンロードが必要となっております。</h>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <h class="eqf">Q 顔出ししなくてもいいですか？</h><br></br>
+                        <br></br>
+                        <h class="eaf">A 会場にお越しいただいた時と同じようにセミナーを進行するため、顔出し必須でお願い致します。他のご参加者の方との交流や学んだことのシェアタイムなどを設けておりますので、積極的にご参加いただき、アウトプットすることでより深い学びが得られます。</h>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <h class="eqf">Q WEBカメラやマイクを持っていません。受講できませんか？</h><br></br>
+                        <br></br>
+                        <h class="eaf">A WEBカメラやマイクをお持ちでない場合も、スマートフォンにてご参加いただけます。安心してご参加ください。</h>
+                    </div>
+                    <stencil-route-link url="#apply"> <img src="/assets/images/fv_btn.png" class="fvbtn2"></img> </stencil-route-link>
+                    <div class="icon5">
+                        <p><font size="25">会社概要</font></p>
+                        <img src="/assets/images/title_bottom.png" class="tb"></img>
+                    </div>
+                    <div class="faq1">
+                        <div class="line"></div>
+                        <p>
+                            <h class="spword">『社会』『人』『創造』の観点から人々に、刺激と感動、学びを与える</h><br></br>
+                            当社は、ビジネス・スキルアップ・教育・マインドセットに関連した勉強会・講演会や<br></br>
+                            ビジネスイベントを企画・運営しています。
+                        </p>
+                    </div>
+                    <div class="cominfo">
+                        <table id="comp"></table>
+                            <tr>社名</tr>  <tr>株式会社ライブクリエイト（Live Create <br></br>co.,ltd)</tr>
+                            <tr>本社</tr> <tr>東京都新宿区西新宿7-7-25<br></br>ワコーレ新宿第2ビル3F</tr>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
